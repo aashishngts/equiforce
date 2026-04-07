15a5a4720d572a99b020b5e913d7cb760a4c9494
Akash Chawda <akashchawda1998@gmail.com>
2026-04-07 16:59:34 +0530

    UPDATE

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -4,19 +4,13 @@ const AboutHero = () => {
   return (
     <section className="pt-35 md:pt-30 lg:pt-30 pb-12 md:pb-10 lg:pb-10 text-center">
       
-      {/* Top */}
-      <div className="inline-flex items-center gap-2 bg-white/80 backdrop-blur-md px-4 py-2 rounded-full text-sm text-gray-800 mb-6 border border-gray-200 shadow-sm">
-        <span className="bg-[#d97706] text-white px-2 py-1 rounded-full text-xs">
-          About
-        </span>
-        Our Expertise & Industry Leadership
-      </div>
+   
 
       <h1 className="text-3xl md:text-5xl font-bold text-gray-800 mb-10">
       Our  Mission
       </h1>
 
-      <p className="text-gray-600 max-w-4xl mx-auto uppercase mb-20 font-medium" >
+      <p className="text-gray-600 max-w-4xl mx-auto  mb-10 font-medium" >
 EquiForce Solutions empowers investment management firms with  <span className="text-[#FF8000]"> purpose-built </span> technology — delivering precision-engineered solutions for investment operations & reconciliation, performance measurement, GIPS composite management, and AI-powered investment reporting. More than a software provider, we are a  <span className="text-[#FF8000]">strategic partner and consultants</span> with nearly three decades of investment operations expertise — integrating <span className="text-[#FF8000]">AI technologies</span> that enable our clients to operate with accuracy, confidence, and a competitive edge.
 
       </p>
